21fe550d422a1d162e1ba1125ab6a5c897649486
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-11-24 01:08:02 -0800

    updates

--- helloworld.py
+++ helloworld.py
@@ -17,3 +17,18 @@ from flask import render_template
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+from flask import request
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if len(request.form.get('username')) > 0:
+            name = request.form.get('username')
+            return render_template('hello.html', name=name, error=error)
+        else:
+            error = 'Invalid username'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('hello.html', error=error)
