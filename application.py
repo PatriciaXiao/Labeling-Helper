b607bfbde64684230667c7ce4b7169509f151744
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-11-24 05:10:24 -0800

    updates

--- application.py
+++ application.py
@@ -68,3 +68,8 @@ def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
--- schema.sql
+++ schema.sql
@@ -1,16 +1,15 @@
-
 CREATE TABLE labels(
     index INT PRIMARY KEY,
-    twitter_id INT,
-    tweet_content VARCHAR(1000),
+    twitter_id TEXT,
+    tweet_content TEXT,
     obama_cnt INT,
     obama_score REAL,
-    trump_cnt INT,
+    trump_cnt TEXT,
     trump_score REAL,
-    obamacare_cnt INT,
+    obamacare_cnt TEXT,
     obamacare_score REAL,
-    trumpcare_cnt INT,
+    trumpcare_cnt TEXT,
     trumpcare_score REAL,
-    republican_cnt INT,
+    republican_cnt TEXT,
     democrats_score REAL
 );
