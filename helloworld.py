6bc69c70357cf916694279e25d486aaa4e766895
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-11-24 00:55:06 -0800

    updates

--- helloworld.py
+++ helloworld.py
@@ -10,3 +10,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
