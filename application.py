0b0e47f08b41963d4b4e29f8fc965ef84cafe7ab
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-11-24 04:16:58 -0800

    Update application.py

--- application.py
+++ application.py
@@ -25,3 +25,29 @@ def showpage():
         predicted_value = "Not yet"
     print(content, keyword, predicted_value)
     return render_template('showpage.html', content_list=[content, content], keyword=keyword)
+
+
+def init_db():
+    print("initialing the data base")
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('debug_schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def debug_print_whole_database():
+    cur = get_db().execute("SELECT * FROM debug WHERE val2 > 5")
+    rv = cur.fetchall()
+    print(rv)
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
