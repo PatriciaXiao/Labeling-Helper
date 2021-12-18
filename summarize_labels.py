e5408e6111baa63174ef6767d161d2c794543994
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-12-17 18:48:58 -0800

    Update summarize_labels.py

--- summarize_labels.py
+++ summarize_labels.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 ifile = "label.csv"
-
 entities = ["obama","trump","obamacare","trumpcare","republican","democrats"]
+# obama_cnt,obama_score,obama_valid
 
 df = pd.read_csv(ifile)
 
